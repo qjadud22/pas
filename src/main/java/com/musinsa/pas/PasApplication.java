b7aa6e9acb0f8ec1d53b4904d26c4f83257d56b0
qjadud22 <qjadud22@gmail.com>
2022-06-25 16:55:26 +0900

    init

---
+++ src/main/java/com/musinsa/pas/PasApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.pas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasApplication.class, args);
+    }
+
+}
